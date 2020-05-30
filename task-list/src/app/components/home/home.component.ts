e321d77ff5407c3ae6a52a557be4b8de13e3d327
draghicistefan <draghicistefan1984@gmail.com>
2020-05-30 11:16:41 +0300

    task list done

--- task-list/src/app/components/home/home.component.ts
+++ task-list/src/app/components/home/home.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit 
+{
+  itemCounter: number=0;
+  projectText: string='angular project';
+  projects=[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    this.itemCounter=this.projects.length;
   }
 
+  addItem()
+  {
+    this.projects.push(this.projectText);
+    this.projectText='';
+    this.itemCounter=this.projects.length;
+  }
+
+  removeItem(i)
+  {
+    this.projects.splice(i, 1);
+    this.itemCounter=this.projects.length;
+  }
 }
