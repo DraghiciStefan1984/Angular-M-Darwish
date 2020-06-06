1c00937675756d6dbf0e10bebee929a5e72aec2e
draghicistefan <draghicistefan1984@gmail.com>
2020-06-06 12:02:09 +0300

    online store pagination

--- online-store/src/app/app.module.ts
+++ online-store/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreComponent } from './components/store/store.component';
 import { StoreModule } from './components/store/store.module';
 
 @NgModule({
--- online-store/src/app/components/store/store.component.ts
+++ online-store/src/app/components/store/store.component.ts
@@ -9,6 +9,10 @@ import { Product } from 'src/app/models/product.model';
 })
 export class StoreComponent implements OnInit 
 {
+  selectedCategory=null;
+  productsPerPage=4;
+  selectedPage=1;
+
   constructor(private productRepository: ProductRepository) {}
 
   ngOnInit(): void 
@@ -17,11 +21,35 @@ export class StoreComponent implements OnInit
 
   get products(): Product[]
   {
-    return this.productRepository.getProducts();
+    let pageIndex=(this.selectedPage-1)*this.productsPerPage;
+    return this.productRepository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex+this.productsPerPage);
   }
 
   get categories(): string[]
   {
     return this.productRepository.getCategories();
+  }
+
+  changeCategory(newCategory?: string)
+  {
+    this.selectedCategory=newCategory;
+  }
+
+  changePage(newPage: number)
+  {
+    this.selectedPage=newPage;
+  }
+
+  changePageSize(newSize: number)
+  {
+    this.productsPerPage=+newSize;
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[]
+  {
+    return Array(Math.ceil(
+      this.productRepository.getProducts(
+        this.selectedCategory).length/this.productsPerPage)).fill(0).map((x, i)=>i+1);
   }
 }
